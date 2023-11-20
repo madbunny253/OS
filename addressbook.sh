07c617316dc4c10dd8ffc43a894082e293456331
Aditya Bodewar <98313615+madbunny253@users.noreply.github.com>
2023-11-20 14:24:44 +0530

    Update addressbook.sh

--- addressbook.sh
+++ addressbook.sh
@@ -1,58 +1,71 @@
 #!/bin/bash
-while true
-do
-	echo -e "\n1. Create Addressbook\n2. Insert record\n3. Display record\n4. Search record\n5. Modify record\n6. Delete Record\n7. Exit\n\nEnter choice"
-	read ch
-	v=1
-	case $ch in
-		1)
-		  echo -e "\nEnter file name :"
-		  read fname
-		  touch $fname.txt
-		  echo -e "$fname.txt file created successfully."
-		  ;;
-		2)
-		  while [ $v -eq 1 ]
-		  do 
-		  	echo -e "\nEnter employee's name :"
-		  	read name
-		  	echo -e "Enter employee number :"
-		  	read eno
-		  	echo -e "Enter employee's mobile number :"
-		  	read mno
-		  	echo -e "Enter employee's city :"
-		  	read city
-		  	echo -e "$name\t$eno\t$mno\t$city" >> $fname.txt
-		  	echo -e "Do you want to continue? Press 1 to continue"
-		  	read v
-		  done
-		  ;;
-		3)
-		  cat emp.txt
-		  ;;
-		4)
-		  echo -e "Enter name that you want to search :"
-		  read ename
-		  grep $ename $fname.txt
-		  ;;
-		5)
-		  echo -e "Enter name that you want to modify :"
-		  read emname
-		  echo -e "Enter new name :"
-		  read empname
-		  sed -i s/$emname/$empname/g $fname.txt
-		  ;;
-		6)
-		  echo -e "Enter name that you want to delete :"
-		  read emplname
-		  grep -v $emplname $fname.txt >> temp.txt
-		  rm $fname.txt
-		  mv temp.txt $fname.txt
-		  ;;
-		7)
-		  exit
-		  ;;
-		*)
-	esac
-done
 
+while true; do
+		echo -e "\n1.Create an address book"
+		echo "2.Insert a record"
+		echo "3.Display a record"
+		echo "4.Delete a record"
+		echo "5.Modify a record"
+		echo "6.Exit"
+		echo "Enter choice :"
+		read ch
+		option=1
+		
+		case $ch in 
+			1)
+				echo "Enter file name"
+				read fname
+				touch $fname
+				echo "file created !"
+			;;
+			
+			2)
+				while [ option -eq 1 ]
+				do
+					echo "Enter employee id: "
+					read eid
+					echo "Enter employee name: "
+					read ename
+					echo "Enter employee phone no.: "
+					read eph
+					echo "Enter employee city: "
+					read ecity
+					
+					echo "$eid $ename $eph $ecity" >> $fname
+					echo "Do you wanna continue? (1-Yes/2-No)"
+					read option
+				done
+			;;
+			
+			3)
+				cat $fname
+			;;
+			
+			4)
+				echo "Enter employee name to delete: "
+				read ename
+				grep -v $ename $fname >> t$fname
+				rm $fname
+				mv t$fname $fname
+				echo "Data deleted !"
+				cat $fname
+			;;
+			
+			5)
+				echo "Enter name of employee to modify: "
+				read ename
+				
+				if grep -v "$ename" "$fname" ; then
+					echo "Enter new name: "
+					read newname
+					sed -i s/$ename/$newname/g "$fname"
+					echo "Record modified successfully :)"
+				else
+					echo "Record not found :("
+				fi
+			
+			6)
+				exit
+			;;
+		esac
+	done
